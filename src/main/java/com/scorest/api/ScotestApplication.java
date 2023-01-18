3d28860afd0a83fbd64910bf8a1052bf5771952e
alireza <alireza.lm69@gmail.com>
2023-01-18 19:51:07 +0100

    init commit

---
+++ src/main/java/com/scorest/api/ScotestApplication.java
@@ -0,0 +1,13 @@
+package com.scorest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScotestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scorest/api/ScotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scorest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScotestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
